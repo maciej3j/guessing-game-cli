2601606b1163fba36c7a19a7796a3a9aaa5c25a5
Maciej K <maciej.ko1444@gmail.com>
2025-01-02 19:34:40 +0100

    the game now keeps track of user's win score divided by each difficulty

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
 import random
 import time
-
+import json
+from tabulate import tabulate
 print('''Welcome to the Number Guessing Game!
 I'm thinking of a number between 1 and 100.
 You have 5 chances to guess the correct number.
 ''')
+
+def read_stats(file="data.json"):
+    try:
+        with open(file, "r") as f:
+            content = json.load(f)
+            return content
+    except (FileNotFoundError, json.JSONDecodeError):
+        return {
+            "Easy": 0,
+            "Medium": 0,
+            "Hard": 0,
+        }
+
+def write_to_stats(difficulty_level, file="data.json"):
+    content = read_stats()
+    content[difficulty_level] += 1
+    with open(file, "w") as f:
+        json.dump(content, f)
 
 def the_game():
     game_difficulty = 0
@@ -49,6 +68,7 @@ def the_game():
             guess = int(input("Enter you guess: "))
             if guess == random_num:
                 print(f"Congratulations! You guessed the correct number in {attempts} attempt" + f"{"s" if attempts != 1 else None}.")
+                write_to_stats(difficulty_name[game_difficulty])
                 break
             elif guess < random_num:
                 attempts += 1
@@ -57,6 +77,12 @@ def the_game():
                 attempts += 1
                 print(f"Incorrect! The number is less than {guess}.")
 
+            if attempts == game_difficulty - 2:
+                random_hint_1 = random.randint(1, 10)
+                random_hint_2 = random.randint(1, 10)
+                print(f"I see you struggling... The number is somewhere between "
+                      f"{random_num-random_hint_1 if random_num-random_hint_1>=0 else 0} and "
+                      f"{random_num+random_hint_2 if random_num+random_hint_2<=100 else 100}.")
             if attempts == game_difficulty:
                 print(f"You failed! All your {game_difficulty} attempts gone. GAME OVER")
                 print(f"The number to guess was {random_num}.")
@@ -67,6 +93,10 @@ def the_game():
     print(f"The guessing game took you PRECISELY {end-start} seconds of your precious lifetime.")
 while True:
     the_game()
-    user_input = input("Press any key to play again or q to quit.")
+    stats = read_stats()
+    print(f"Your current stats:")
+    for key, val in stats.items():
+        print(f"{key}: {val}")
+    user_input = input("\nPress any key to play again or q to quit. \n")
     if user_input == "q":
         break
